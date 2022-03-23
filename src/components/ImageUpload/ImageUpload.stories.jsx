13d2749c4377c6fce8b95c95ac4809c2b461bd5d
Ajayi Peter <peterdgreat2016@gmail.com>
2022-03-23 21:30:37 +0100

    Created image upload component

---
+++ src/components/ImageUpload/ImageUpload.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ImageUpload from './ImageUpload';
+
+export default {
+  title: 'Component/ImageUpload',
+  component: ImageUpload,
+};
+
+const Template = () => <ImageUpload />;
+
+export const imageupload = Template.bind({});
