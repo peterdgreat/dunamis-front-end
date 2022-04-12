ad94204b6365f44e954e479fd4b624b3971eb2ab
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-12 13:38:42 +0200

    removed unused codes

--- src/pages/Login/AdminLogin.jsx
+++ src/pages/Login/AdminLogin.jsx
@@ -1,16 +1,52 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from '../../components/InputField/InputField';
+import { login } from '../../redux/login/login';
 
 export default function AdminLogin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [Response, setResponse] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const postData = new FormData();
+    postData.append('email', email);
+    postData.append('password', password);
+    const response = await login(postData);
+    setResponse('Hello');
+    console.log(response);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {Response}
       <h3>Sign In</h3>
       <div className="form-group">
-        <InputField type="email" className="form-control" placeholder="Enter email" label="Email" />
+        <InputField
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          label="Email"
+          onChange={
+              (e) => {
+                setEmail(e.target.value);
+              }
+        }
+        />
       </div>
       <div className="form-group">
-        <InputField type="password" className="form-control" placeholder="Enter password" label="Passwsord" />
+        <InputField
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          label="Passwsord"
+          onChange={
+              (e) => {
+                setPassword(e.target.value);
+              }
+        }
+        />
       </div>
       {/* <div className="form-group">
         <div className="custom-control custom-checkbox">
