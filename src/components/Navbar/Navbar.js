8b4a5ae7f9313068426a023643a51175c3c2c73e
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-27 19:06:38 +0200

    nav toggle fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 // import { logout } from '../redux/logout/logout';
 import { useSelector } from 'react-redux';
+import hamburger from './assets/hamburger.png';
 
 export default function NavBar() {
   const admin = useSelector((state) => state.admin);
@@ -13,27 +14,29 @@ export default function NavBar() {
     window.location.href = '/';
   };
   return (
-    <nav className="navbar">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
-          Dunamis Photography
-        </NavLink>
-      </div>
+    <nav className="navbar fixed-top navbar-expand-lg justify-content-between  text-white d-flex container-fluid">
+      <NavLink className="navbar-brand  text-white" to="/">
+        DUNAMIS PHOTOGRAPHY
+      </NavLink>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <img src={hamburger} alt="hamburger" />
+      </button>
       {
               admin?.admin?.id && (
-                <ul>
-                  <li className="nav-item bdr d-flex align-items-center px-3">
-                    <span role="button" className="nav-Link pt-1" onClick={handleLogout}>
-                      Logout
-                    </span>
-                  </li>
-                  <li className="nav-item bdr d-flex align-items-center px-3">
-                    <NavLink className="nav-Link pt-1" to="/PDG_admin/dashboard">
-                      Dashboard
-                    </NavLink>
-                  </li>
-                </ul>
+                <div className="collapse justify-content-end navbar-collapse" id="navbarSupportedContent">
+                  <ul className="d-flex ">
+                    <li className="nav-item bdr d-flex align-items-center px-3">
+                      <NavLink className="nav-Link btn btn-dark  text-white" to="/PDG_admin/dashboard">
+                        Dashboard
+                      </NavLink>
+                    </li>
+                    <li className="nav-item bdr d-flex align-items-center px-3">
+                      <span role="button" className=" btn nav-Link btn-danger" onClick={handleLogout}>
+                        Logout
+                      </span>
+                    </li>
+                  </ul>
+                </div>
               )
             }
     </nav>
