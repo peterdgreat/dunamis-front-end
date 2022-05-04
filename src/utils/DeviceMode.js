e14e88893acda43eb0488876da545ee2ae357c79
Ajayi Peter <peterdgreat2016@gmail.com>
2022-05-04 03:13:40 +0200

    disabled default import

--- src/utils/DeviceMode.js
+++ src/utils/DeviceMode.js
@@ -1,24 +1,25 @@
-import React from "react";
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
 
 function useDeviceMode() {
-    const [deviceMode, setDeviceInfo] = React.useState({
-        desktop: window.innerWidth > 768 ? true : false
-    });
+  const [deviceMode, setDeviceInfo] = React.useState({
+    desktop: window.innerWidth > 768,
+  });
 
-    function updateDevice(mql) {
-        setDeviceInfo(prev => ({...prev, desktop: mql.matches}));
-    }
+  function updateDevice(mql) {
+    setDeviceInfo((prev) => ({ ...prev, desktop: mql.matches }));
+  }
 
-    React.useEffect(() => {
-        let mediaQuery = window.matchMedia("(min-width: 768px)");
-        mediaQuery.addEventListener("change", updateDevice);
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 768px)');
+    mediaQuery.addEventListener('change', updateDevice);
 
-        return () => {
-            mediaQuery.removeEventListener("change", updateDevice);
-        };
-    }, []);
+    return () => {
+      mediaQuery.removeEventListener('change', updateDevice);
+    };
+  }, []);
 
-    return deviceMode;
+  return deviceMode;
 }
 
 export { useDeviceMode };
