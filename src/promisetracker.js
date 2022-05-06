6b1a5d3ea852359e533fb3139409d4c6dd129138
Ajayi Peter <peterdgreat2016@gmail.com>
2022-05-07 00:07:11 +0200

    added promise tracker component

---
+++ src/promisetracker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { usePromiseTracker } from 'react-promise-tracker';
+
+export default function promisetracker() {
+  const { promiseInProgress } = usePromiseTracker();
+  return (
+    <div>
+      {promiseInProgress
+    && <h1>Hey some async call in progress ! </h1>}
+    </div>
+  );
+}
