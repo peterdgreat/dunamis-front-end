4c616f68a156dba5c34179281348fbf1f3236024
Ajayi Peter <peterdgreat2016@gmail.com>
2022-05-06 19:26:48 +0200

    created portfolio page

---
+++ src/pages/Portfolio/Portfolio.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+background-color: #32273B;
+width: 100%;
+height: 100%;
+padding-top: 50px;
+padding-left: 8%;
+padding-right: 8%;
+`;
+
+export default Container;
